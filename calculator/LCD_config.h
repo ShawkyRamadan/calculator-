2cb4e055e25b528df5a980c33500afb595f150d5
Shawky Ramadan <161169216+ShawkyRamadan@users.noreply.github.com>
2024-03-14 22:32:06 +0200

    Add files via upload

---
+++ calculator/LCD_config.h
@@ -0,0 +1,12 @@
+/*
+ * LCD_config.h
+ *
+ * Created: 5/24/2023 4:06:22 PM
+ *  Author: Shawky Ramadan
+ */ 
+
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+#define eight_bits_mode
+#endif /* LCD_CONFIG_H_ */
